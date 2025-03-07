ce866ad6af978b789672dd27905369492d4542b9
nctu0513325 <65332776+nctu0513325@users.noreply.github.com>
2025-03-08 00:37:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,26 @@
 from mininet.topo import Topo
 
-class LinearTopo(Topo):
-    def build(self, k=3):
-        # 創建 k 個交換機
-        switches = [self.addSwitch('s%d' % (i + 1)) for i in range(k)]
-        
-        # 創建 k 個主機並連接到對應的交換機
-        for i in range(k):
-            host = self.addHost('h%d' % (i + 1))
-            self.addLink(host, switches[i])
-        
-        # 連接交換機形成線性拓撲
-        for i in range(k - 1):
-            self.addLink(switches[i], switches[i + 1])
+class MyLinearTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
 
-topos = {'linear': LinearTopo}
+        # Add hosts
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+
+        # Add switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+
+        # Add links between hosts and switches
+        self.addLink(h1, s1)
+        self.addLink(h2, s2)
+        self.addLink(h3, s3)
+
+        # Add links between switches to form a linear topology
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+
+topos = {'mylineartopo': MyLinearTopo}
