df34813cca0b22cc2a5e5a9ee7cdb059b807eacb
nctu0513325 <65332776+nctu0513325@users.noreply.github.com>
2025-04-18 15:37:09 +0800

    Update hw3.c

--- hw3.c
+++ hw3.c
@@ -1,65 +1,53 @@
-#include "bfs.h"
-#include <cstdlib>
-#include <omp.h>
-#include <vector>
-#include "../common/graph.h"
-
-#ifdef VERBOSE
-#include "../common/CycleTimer.h"
-#include <stdio.h>
-#endif
-
-constexpr int ROOT_NODE_ID = 0;
-constexpr int NOT_VISITED_MARKER = -1;
-
-// 原有的top-down相關函數保持不變
-
-void bottom_up_step(Graph g, std::vector<bool>& frontier, std::vector<bool>& new_frontier, int* distances, int level)
-{
-    #pragma omp parallel for
-    for (int i = 0; i < g->num_nodes; i++)
-    {
-        if (distances[i] == NOT_VISITED_MARKER)
-        {
-            for (int j = g->incoming_starts[i]; j < (i == g->num_nodes - 1 ? g->num_edges : g->incoming_starts[i + 1]); j++)
-            {
-                int incoming = g->incoming_edges[j];
-                if (frontier[incoming])
-                {
-                    distances[i] = level;
-                    new_frontier[i] = true;
-                    break;
-                }
-            }
-        }
-    }
-}
-
 void bfs_bottom_up(Graph graph, solution* sol)
 {
-    std::vector<bool> frontier(graph->num_nodes, false);
-    std::vector<bool> new_frontier(graph->num_nodes, false);
+    bool* frontier = new bool[graph->num_nodes]();
+    bool* new_frontier = new bool[graph->num_nodes]();
 
-    // 初始化所有節點為未訪問
+    #pragma omp parallel for
     for (int i = 0; i < graph->num_nodes; i++)
         sol->distances[i] = NOT_VISITED_MARKER;
 
-    // 設置根節點
     frontier[ROOT_NODE_ID] = true;
     sol->distances[ROOT_NODE_ID] = 0;
 
     int level = 1;
-    while (true)
+    bool done = false;
+
+    while (!done)
     {
-        bottom_up_step(graph, frontier, new_frontier, sol->distances, level);
+        done = true;
 
-        if (std::all_of(new_frontier.begin(), new_frontier.end(), [](bool v) { return !v; }))
-            break;
+        #pragma omp parallel for reduction(&&:done)
+        for (int i = 0; i < graph->num_nodes; i++)
+        {
+            if (sol->distances[i] == NOT_VISITED_MARKER)
+            {
+                for (int j = graph->incoming_starts[i]; j < (i == graph->num_nodes - 1 ? graph->num_edges : graph->incoming_starts[i + 1]); j++)
+                {
+                    int incoming = graph->incoming_edges[j];
+                    if (frontier[incoming])
+                    {
+                        sol->distances[i] = level;
+                        new_frontier[i] = true;
+                        done = false;
+                        break;
+                    }
+                }
+            }
+        }
 
-        frontier.swap(new_frontier);
-        std::fill(new_frontier.begin(), new_frontier.end(), false);
+        #pragma omp parallel for
+        for (int i = 0; i < graph->num_nodes; i++)
+        {
+            frontier[i] = new_frontier[i];
+            new_frontier[i] = false;
+        }
+
         level++;
     }
+
+    delete[] frontier;
+    delete[] new_frontier;
 }
 
 void bfs_hybrid(Graph graph, solution* sol)
@@ -71,24 +59,25 @@ void bfs_hybrid(Graph graph, solution* sol)
     VertexSet* frontier = &list1;
     VertexSet* new_frontier = &list2;
 
-    std::vector<bool> bottom_up_frontier(graph->num_nodes, false);
-    std::vector<bool> bottom_up_new_frontier(graph->num_nodes, false);
+    bool* bottom_up_frontier = new bool[graph->num_nodes]();
+    bool* bottom_up_new_frontier = new bool[graph->num_nodes]();
 
-    // 初始化所有節點為未訪問
+    #pragma omp parallel for
     for (int i = 0; i < graph->num_nodes; i++)
         sol->distances[i] = NOT_VISITED_MARKER;
 
-    // 設置根節點
     frontier->vertices[frontier->count++] = ROOT_NODE_ID;
     bottom_up_frontier[ROOT_NODE_ID] = true;
     sol->distances[ROOT_NODE_ID] = 0;
 
     int level = 1;
     bool use_top_down = true;
+    bool done = false;
 
-    while (frontier->count != 0 || std::any_of(bottom_up_frontier.begin(), bottom_up_frontier.end(), [](bool v) { return v; }))
+    while (!done)
     {
-        // 根據frontier的大小決定使用top-down還是bottom-up
+        done = true;
+
         if (frontier->count < graph->num_nodes / 20)
             use_top_down = true;
         else if (frontier->count > graph->num_nodes / 5)
@@ -97,7 +86,42 @@ void bfs_hybrid(Graph graph, solution* sol)
         if (use_top_down)
         {
             vertex_set_clear(new_frontier);
-            top_down_step(graph, frontier, new_frontier, sol->distances);
+
+            #pragma omp parallel
+            {
+                VertexSet local_new_frontier;
+                vertex_set_init(&local_new_frontier, graph->num_nodes);
+
+                #pragma omp for
+                for (int i = 0; i < frontier->count; i++)
+                {
+                    int node = frontier->vertices[i];
+                    int start_edge = graph->outgoing_starts[node];
+                    int end_edge = (node == graph->num_nodes - 1) ? graph->num_edges : graph->outgoing_starts[node + 1];
+
+                    for (int neighbor = start_edge; neighbor < end_edge; neighbor++)
+                    {
+                        int outgoing = graph->outgoing_edges[neighbor];
+                        if (__sync_bool_compare_and_swap(&sol->distances[outgoing], NOT_VISITED_MARKER, level))
+                        {
+                            local_new_frontier.vertices[local_new_frontier.count++] = outgoing;
+                        }
+                    }
+                }
+
+                #pragma omp critical
+                {
+                    for (int i = 0; i < local_new_frontier.count; i++)
+                    {
+                        new_frontier->vertices[new_frontier->count++] = local_new_frontier.vertices[i];
+                    }
+                }
+
+                vertex_set_destroy(&local_new_frontier);
+            }
+
+            if (new_frontier->count > 0)
+                done = false;
 
             VertexSet* tmp = frontier;
             frontier = new_frontier;
@@ -105,18 +129,56 @@ void bfs_hybrid(Graph graph, solution* sol)
         }
         else
         {
-            bottom_up_step(graph, bottom_up_frontier, bottom_up_new_frontier, sol->distances, level);
-            bottom_up_frontier.swap(bottom_up_new_frontier);
-            std::fill(bottom_up_new_frontier.begin(), bottom_up_new_frontier.end(), false);
-
-            // 將bottom-up frontier轉換為top-down frontier
-            vertex_set_clear(frontier);
+            #pragma omp parallel for reduction(&&:done)
             for (int i = 0; i < graph->num_nodes; i++)
             {
-                if (bottom_up_frontier[i])
+                if (sol->distances[i] == NOT_VISITED_MARKER)
                 {
-                    frontier->vertices[frontier->count++] = i;
+                    for (int j = graph->incoming_starts[i]; j < (i == graph->num_nodes - 1 ? graph->num_edges : graph->incoming_starts[i + 1]); j++)
+                    {
+                        int incoming = graph->incoming_edges[j];
+                        if (bottom_up_frontier[incoming])
+                        {
+                            sol->distances[i] = level;
+                            bottom_up_new_frontier[i] = true;
+                            done = false;
+                            break;
+                        }
+                    }
                 }
+            }
+
+            #pragma omp parallel for
+            for (int i = 0; i < graph->num_nodes; i++)
+            {
+                bottom_up_frontier[i] = bottom_up_new_frontier[i];
+                bottom_up_new_frontier[i] = false;
+            }
+
+            vertex_set_clear(frontier);
+            #pragma omp parallel
+            {
+                VertexSet local_frontier;
+                vertex_set_init(&local_frontier, graph->num_nodes);
+
+                #pragma omp for
+                for (int i = 0; i < graph->num_nodes; i++)
+                {
+                    if (bottom_up_frontier[i])
+                    {
+                        local_frontier.vertices[local_frontier.count++] = i;
+                    }
+                }
+
+                #pragma omp critical
+                {
+                    for (int i = 0; i < local_frontier.count; i++)
+                    {
+                        frontier->vertices[frontier->count++] = local_frontier.vertices[i];
+                    }
+                }
+
+                vertex_set_destroy(&local_frontier);
             }
         }
 
@@ -125,4 +187,6 @@ void bfs_hybrid(Graph graph, solution* sol)
 
     vertex_set_destroy(&list1);
     vertex_set_destroy(&list2);
+    delete[] bottom_up_frontier;
+    delete[] bottom_up_new_frontier;
 }
