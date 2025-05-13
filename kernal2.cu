9a559c8d965cf7f89302a29874b0d4d40cc53f1e
nctu0513325 <65332776+nctu0513325@users.noreply.github.com>
2025-05-13 17:24:28 +0800

    Create kernal2.cu

---
+++ kernal2.cu
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <cstdlib>
+#include "mandel.h"
+
+__global__ void mandel_kernel(float lower_x, float lower_y, float step_x, float step_y, int *img, int res_x, int res_y, int max_iterations) {
+    int thisX = blockIdx.x * blockDim.x + threadIdx.x;
+    int thisY = blockIdx.y * blockDim.y + threadIdx.y;
+    if (thisX < res_x && thisY < res_y) {
+        float x = lower_x + thisX * step_x;
+        float y = lower_y + thisY * step_y;
+        // Compute mandelbrot value and store in img
+    }
+}
+
+void host_fe(float upper_x, float upper_y, float lower_x, float lower_y, int *img, int res_x, int res_y, int max_iterations) {
+    float step_x = (upper_x - lower_x) / (float)res_x;
+    float step_y = (upper_y - lower_y) / (float)res_y;
+
+    int *host_img;
+    cudaHostAlloc(&host_img, res_x * res_y * sizeof(int), cudaHostAllocDefault);
+
+    int *device_img;
+    size_t pitch;
+    cudaMallocPitch(&device_img, &pitch, res_x * sizeof(int), res_y);
+
+    dim3 threadsPerBlock(16, 16);
+    dim3 numBlocks((res_x + threadsPerBlock.x - 1) / threadsPerBlock.x, (res_y + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    mandel_kernel<<<numBlocks, threadsPerBlock>>>(lower_x, lower_y, step_x, step_y, device_img, res_x, res_y, max_iterations);
+
+    cudaMemcpy2D(host_img, res_x * sizeof(int), device_img, pitch, res_x * sizeof(int), res_y, cudaMemcpyDeviceToHost);
+
+    // Copy host_img to img
+    for (int i = 0; i < res_x * res_y; ++i) {
+        img[i] = host_img[i];
+    }
+
+    cudaFreeHost(host_img);
+    cudaFree(device_img);
+}
